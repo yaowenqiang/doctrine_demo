55e47e128e13d21662366bc09e4cd93f561456ad
yaowenqiang <yaowenqiang111@163.com>
2020-03-31 02:51:01 +0800

    add query builder demo

--- src/AppBundle/Controller/FortuneController.php
+++ src/AppBundle/Controller/FortuneController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Category;
@@ -11,13 +12,19 @@ class FortuneController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function homepageAction()
+    public function homepageAction(Request $request)
     {
         $categoryRepository = $this->getDoctrine()
             ->getManager()
             ->getRepository('AppBundle:Category');
 
-        $categories = $categoryRepository->findAllOrdered();
+        $search = $request->query->get('q');
+        if ($search) {
+            $categories = $categoryRepository->search($search);
+        } else {
+            $categories = $categoryRepository->findAllOrdered();
+        }
+
 
         return $this->render('fortune/homepage.html.twig',[
             'categories' => $categories
--- src/AppBundle/Entity/CategoryRepository.php
+++ src/AppBundle/Entity/CategoryRepository.php
@@ -14,15 +14,31 @@ class CategoryRepository extends EntityRepository
 {
     public function findAllOrdered()
     {
-        $dql = "select cat from AppBundle\Entity\Category cat order  by cat.name desc";
+//        $dql = "select cat from AppBundle\Entity\Category cat order  by cat.name desc";
 
-        $query = $this->getEntityManager()->createQuery($dql);
+        $qb = $this->createQueryBuilder('cat')
+            ->addOrderBy('cat.name', 'DESC');
+
+        $query = $qb->getQuery();
+//        var_dump($query->getDQL());
+
+
+
+
+//        $query = $this->getEntityManager()->createQuery($dql);
 //        var_dump($query->getSQL());
 
         return $query->execute();
 
+    }
 
-
-
+    public function search($term)
+    {
+        return $this->createQueryBuilder('cat')
+//            ->andWhere('cat.name = :searchItem')
+            ->andWhere('cat.name LIKE :searchItem OR cat.iconKey like :searchItem')
+            ->setParameter('searchItem', '%' . $term . '%')
+            ->getQuery()
+            ->execute();
     }
 }
